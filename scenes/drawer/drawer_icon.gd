35eee7822eef495b2dedd0f5ef5010dae41c6f49
Bytez <25850701+Bytezz@users.noreply.github.com>
2023-04-13 05:12:47 +0200

    Release focus from nodes on window focus out.
    Releasing focus on focus out and grabbing it again on focus in.
    This avoid activating UI with gamepad while window not in focus.

--- scenes/drawer/drawer_icon.gd
+++ scenes/drawer/drawer_icon.gd
@@ -4,5 +4,4 @@ var game : Dictionary
 var drawer_row : Node
 
 func _on_Button_pressed():
-	# FIXME: Activate ui_accept with gamepad execute multiple times
 	LutrisApi.run_game(game)
--- scenes/home/home.gd
+++ scenes/home/home.gd
@@ -1,7 +1,23 @@
 extends Control
 
+var old_focused = null
+
 func _ready():
 	$Black.show()
 	Fader.fade_out($Black)
 	
 	get_tree().get_nodes_in_group("IconLaunchers")[0].grab_focus()
+
+func _notification(what):
+	match what:
+		# Avoid activating UI with gamepad while window not in focus
+		MainLoop.NOTIFICATION_WM_FOCUS_OUT:
+			var tmp = get_focus_owner()
+			if not tmp == null:
+				old_focused = tmp
+				print(old_focused)
+				old_focused.release_focus()
+		MainLoop.NOTIFICATION_WM_FOCUS_IN:
+			if not old_focused == null:
+				old_focused.grab_focus()
+				old_focused = null
